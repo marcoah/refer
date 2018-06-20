8dbdda9b581d64dd3efd75fbd5fe97f1621c5a83
Marco Antonio <marcoah@gmail.com>
2018-06-19 21:05:14 -0400

    Agregado refer que estaba en local

---
+++ tablarefer.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+<title>Ver tabla REFER</title>
+</head>
+<body>
+<h1>Lista de Piezas con Stock > 0</h1>
+<hr>
+<table border="1" cellspacing=1 cellpadding=2 style="font-size: 8pt"><tr>
+<td><b>Código</b></font></td>
+<td><b>Descripción</b></font></td>
+<td><b>Cantidad</b></font></td>
+<td><b>Precio</b></font></td>
+</tr>
+
+<?php
+$mysqli = new mysqli('127.0.0.1', 'root', '', 'refer');
+if ($mysqli->connect_errno) {
+    echo "Lo sentimos, este sitio web está experimentando problemas.";
+    echo "Error: Fallo al conectarse a MySQL debido a: \n";
+    echo "Errno: " . $mysqli->connect_errno . "\n";
+    echo "Error: " . $mysqli->connect_error . "\n";
+    exit;
+}
+
+$sql = "SELECT refer.NPRO, refer.DPRO, refer.STOC, refer.BPNC FROM refer WHERE refer.STOC > 0 ";
+if (!$resultado = $mysqli->query($sql)) {
+    echo "Lo sentimos, este sitio web está experimentando problemas.";
+    echo "Error: La ejecución de la consulta falló debido a: \n";
+    echo "Query: " . $sql . "\n";
+    echo "Errno: " . $mysqli->errno . "\n";
+    echo "Error: " . $mysqli->error . "\n";
+    exit;
+}
+if ($resultado->num_rows === 0) {
+    echo "Lo sentimos. No se pudo encontrar una coincidencia para el ID. Inténtelo de nuevo.";
+    exit;
+}
+
+echo "<ul>\n";
+while ($row = $resultado->fetch_assoc()) {
+
+    echo "<tr><td width=\"25%\"><font face=\"verdana\">" .$row["NPRO"] . "</font></td>";
+    echo "<td width=\"25%\"><font face=\"verdana\">" .$row["DPRO"] . "</font></td>";
+    echo "<td width=\"25%\"><font face=\"verdana\">" .$row["STOC"] . "</font></td>";
+    echo "<td width=\"25%\"><font face=\"verdana\">" .$row["BPNC"]. "</font></td></tr>";
+}
+$numero = $resultado->num_rows;
+echo "<tr><td colspan=\"15\"><font face=\"verdana\"><b>Número: " . $numero . "</b></font></td></tr>";
+echo "</ul>\n";
+
+$resultado->free();
+$mysqli->close();
+?>
+
+</table>
+
+</body>
+</html>
