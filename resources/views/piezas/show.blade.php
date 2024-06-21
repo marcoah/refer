901c20ff0ec6b177f2085a65aef563297146d274
Marco Hernandez <marcoah@outlook.com>
2024-06-21 14:09:13 -0400

    Ajustes

--- resources/views/piezas/show.blade.php
+++ resources/views/piezas/show.blade.php
@@ -7,16 +7,16 @@
 @section('content')
 
 <div class="pagetitle">
-    <h1>Dashboard</h1>
+    <h1>Title</h1>
     <nav>
       <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-        <li class="breadcrumb-item active">Dashboard</li>
+        <li class="breadcrumb-item"><a href="{{ route('home') }}">Inicio</a></li>
+        <li class="breadcrumb-item active">Title</li>
       </ol>
     </nav>
 </div><!-- End Page Title -->
 
-<section class="section dashboard">
+<section class="section">
     <div class="row">
     </div>
 </section>
