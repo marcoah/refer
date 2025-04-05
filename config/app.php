64558ac9d407a0d18d1072234def734d6388f227
Marco Hernandez <marcoah@outlook.com>
2025-04-04 21:03:43 -0300

    Remove Yajra DataTables service provider from application configuration

--- config/app.php
+++ config/app.php
@@ -161,7 +161,6 @@ return [
          */
         Maatwebsite\Excel\ExcelServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
-        Yajra\DataTables\DataTablesServiceProvider::class,
 
         /*
          * Application Service Providers...
