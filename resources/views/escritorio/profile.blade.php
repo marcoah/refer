3bbbcbe27d5b5ab3095851b01bbf2e96fba4abc7
Marco Hernandez <marcoah@outlook.com>
2025-08-13 19:54:42 -0300

    Update profile view to dynamically display user information

--- resources/views/escritorio/profile.blade.php
+++ resources/views/escritorio/profile.blade.php
@@ -72,37 +72,37 @@
 
                     <div class="row">
                     <div class="col-lg-3 col-md-4 label ">Full Name</div>
-                    <div class="col-lg-9 col-md-8">Kevin Anderson</div>
+                    <div class="col-lg-9 col-md-8">{{ $user->fullname }}</div>
                     </div>
 
                     <div class="row">
                     <div class="col-lg-3 col-md-4 label">Company</div>
-                    <div class="col-lg-9 col-md-8">Lueilwitz, Wisoky and Leuschke</div>
+                    <div class="col-lg-9 col-md-8">{{ $user->profile->profile_company }}</div>
                     </div>
 
                     <div class="row">
                     <div class="col-lg-3 col-md-4 label">Job</div>
-                    <div class="col-lg-9 col-md-8">Web Designer</div>
+                    <div class="col-lg-9 col-md-8">{{ $user->profile->profile_job }}</div>
                     </div>
 
                     <div class="row">
                     <div class="col-lg-3 col-md-4 label">Country</div>
-                    <div class="col-lg-9 col-md-8">USA</div>
+                    <div class="col-lg-9 col-md-8">{{ $user->profile->profile_country }}</div>
                     </div>
 
                     <div class="row">
                     <div class="col-lg-3 col-md-4 label">Address</div>
-                    <div class="col-lg-9 col-md-8">A108 Adam Street, New York, NY 535022</div>
+                    <div class="col-lg-9 col-md-8">{{ $user->profile->profile_address }}</div>
                     </div>
 
                     <div class="row">
                     <div class="col-lg-3 col-md-4 label">Phone</div>
-                    <div class="col-lg-9 col-md-8">(436) 486-3538 x29071</div>
+                    <div class="col-lg-9 col-md-8">{{ $user->profile->profile_phone }}</div>
                     </div>
 
                     <div class="row">
                     <div class="col-lg-3 col-md-4 label">Email</div>
-                    <div class="col-lg-9 col-md-8">k.anderson@example.com</div>
+                    <div class="col-lg-9 col-md-8">{{ $user->email }}</div>
                     </div>
 
                 </div>
@@ -114,7 +114,7 @@
                     <div class="row mb-3">
                         <label for="profileImage" class="col-md-4 col-lg-3 col-form-label">Profile Image</label>
                         <div class="col-md-8 col-lg-9">
-                        <img src="assets/img/profile-img.jpg" alt="Profile">
+                        <img src="{{ $user->avatar_url }}" alt="Profile">
                         <div class="pt-2">
                             <a href="#" class="btn btn-primary btn-sm" title="Upload new profile image"><i class="bi bi-upload"></i></a>
                             <a href="#" class="btn btn-danger btn-sm" title="Remove my profile image"><i class="bi bi-trash"></i></a>
@@ -125,7 +125,7 @@
                     <div class="row mb-3">
                         <label for="fullName" class="col-md-4 col-lg-3 col-form-label">Full Name</label>
                         <div class="col-md-8 col-lg-9">
-                        <input name="fullName" type="text" class="form-control" id="fullName" value="{{ $user->getfullname() }}">
+                        <input name="fullName" type="text" class="form-control" id="fullName" value="{{ $user->fullname }}">
                         </div>
                     </div>
 
@@ -139,7 +139,7 @@
                     <div class="row mb-3">
                         <label for="company" class="col-md-4 col-lg-3 col-form-label">Company</label>
                         <div class="col-md-8 col-lg-9">
-                        <input name="company" type="text" class="form-control" id="company" value="Lueilwitz, Wisoky and Leuschke">
+                        <input name="company" type="text" class="form-control" id="company" value="{{ $user->profile->profile_company }}">
                         </div>
                     </div>
 
@@ -153,14 +153,14 @@
                     <div class="row mb-3">
                         <label for="Country" class="col-md-4 col-lg-3 col-form-label">Country</label>
                         <div class="col-md-8 col-lg-9">
-                        <input name="country" type="text" class="form-control" id="Country" value="USA">
+                        <input name="country" type="text" class="form-control" id="Country" value="{{ $user->profile->profile_country }}">
                         </div>
                     </div>
 
                     <div class="row mb-3">
                         <label for="Address" class="col-md-4 col-lg-3 col-form-label">Address</label>
                         <div class="col-md-8 col-lg-9">
-                        <input name="address" type="text" class="form-control" id="Address" value="A108 Adam Street, New York, NY 535022">
+                        <input name="address" type="text" class="form-control" id="Address" value="{{ $user->profile->profile_address }}">
                         </div>
                     </div>
 
